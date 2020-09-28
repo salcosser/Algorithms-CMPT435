43a1eac4fdf12814f1e77aca3e863d6014fd1541
Samuel Alcosser <samalcosser@gmail.com>
2020-09-27 20:24:55 -0400

    Cleaned up repo

--- Assignment_2/temp/Sort.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-class Sort {
-	static void Insertion(string &s);
-	/*static void Merge(string s);
-	static void Selection(string s);
-	static void Quick(string s);*/
-}
