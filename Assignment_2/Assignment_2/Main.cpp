9f331e61bcf78f1a10d36176cf54041b3af7ac4f
Samuel Alcosser <samalcosser@gmail.com>
2020-09-30 17:40:52 -0400

    Cleaning Up

--- Assignment_2/Assignment_2/Main.cpp
+++ Assignment_2/Assignment_2/Main.cpp
@@ -2,39 +2,33 @@
 #include <string>
 #include <fstream>
 #include "Sort.h"
-#include <algorithm> 
+#include <algorithm>
 #include <thread>
 #include <chrono>
 
-
-
-
-const int len = 666;
+const int len = 666; //normal mode  finishes in ~15 - 35 ms
 
 std::string arr[4][len];
 //std::string arr[len];
 void setup()
 {
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++)
+	{
 		std::fstream newfile;
 
 		std::string ln;
 		int count = 0;
 
-		newfile.open("magicitems.txt", std::ios::in);
+		newfile.open("magicitems.txt", std::ios::in); //normal mode
 		if (newfile.is_open())
 		{
-			while (getline(newfile, ln)) {
+			while (getline(newfile, ln))
+			{
 
 				transform(ln.begin(), ln.end(), ln.begin(), ::tolower);
 
 				arr[i][count++] = ln;
-				//arr[count++] = ln;
 			}
-
-
-
-
 		}
 		newfile.close();
 	}
@@ -42,86 +36,18 @@ void setup()
 	return;
 }
 
-/* void reset(){
-	for(std::string s : arr){
-		s = "";
-	}
-}
- */
-
-
-
-
-
-int main() {
+int main()
+{
 	setup();
-	std::cout << "Insertion sort made " << std::to_string(Sort::Insertion(arr[0], len)) << " comparisons."<<std::endl;
+	std::cout << "Insertion sort made " << std::to_string(Sort::Insertion(arr[0], len)) << " comparisons." << std::endl;
 
-	std::cout << "Selection sort made " << std::to_string(Sort::Selection(arr[1], len)) << " comparisons." <<std::endl;
+	std::cout << "Selection sort made " << std::to_string(Sort::Selection(arr[1], len)) << " comparisons." << std::endl;
 
 	int cnt = 0;
 	Sort::mergeSort(arr[2], 0, (len - 1), cnt);
 	std::cout << "Merge sort made " << std::to_string(cnt) << " comparisons." << std::endl;
-	
+
 	int qCnt = 0;
 	Sort::quickSort(arr[3], 0, (len - 1), qCnt);
 	std::cout << "Quick sort made " << std::to_string(qCnt) << " comparisons." << std::endl;
-	
-
-
-
-
-
-
-
-
-	//Beware, I attempt muli-threading below
-
-
-
-
-
-
-
-
-	//int cnt = 0, qCnt = 0, cnt1 = 0, cnt2 = 0;
-
-
-
-
-
-//auto  insert = []( std::string arr[], int len, int &cnt) { 
-//        Sort::Insertion(arr, len, cnt);
-//    }; 
-//auto select = []( std::string arr[], int len, int &cnt) { 
-//        Sort::Selection(arr, len, cnt);
-//    }; 
-//auto merge = []( std::string arr[], int start, int end, int &cnt) { 
-//        Sort::mergeSort(arr, start, end,  cnt);
-//    }; 
-//auto quick =   []( std::string arr[], int start, int end,  int &cnt) { 
-//        Sort::quickSort(arr, start, end, cnt);
-//    }; 
-//
-//    // This thread is launched by using  
-//    // lamda expression as callable 
-//
-//
-//
-//	 std::thread t1(insert, arr[0], len, cnt1);
-//	 	
-//	 std::thread t2(select, arr[1], len, cnt2);
-//	
-//	 std::thread t3(merge, arr[2], 0, (len - 1), cnt);
-//	 
-//	 std::thread t4(quick, arr[3], 0, (len - 1), qCnt);
-//	t1.join();
-//	t2.join();
-//	t3.join();
-//    t4.join();
-//	std::cout << "Insertion Sort: " << std::to_string(cnt1) << std::endl;
-//	std::cout << "Selection Sort: " << std::to_string(cnt2) << std::endl;
-//	std::cout << "Merge Sort: " << std::to_string(cnt) << std::endl;
-//	std::cout << "Quick Sort: " << std::to_string(qCnt) << std::endl;
-
 }
--- Assignment_2/Assignment_2/Sort.cpp
+++ Assignment_2/Assignment_2/Sort.cpp
@@ -4,14 +4,17 @@
 
 #include <algorithm>
 
-void Sort::swap(std::string& a, std::string& b) {
-	std::string  temp = a;
+void Sort::swap(std::string &a, std::string &b)
+{
+	std::string temp = a;
 	a = b;
 	b = temp;
 }
 
-void Sort::printList(std::string s[], int n) {
-	for (int i = 0; i < n; i++) {
+void Sort::printList(std::string s[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
 		std::cout << s[i] << std::endl;
 	}
 }
@@ -27,12 +30,12 @@ int Sort::Insertion(std::string s[], int n)
 		while (j >= 0 && s[j] > last)
 		{
 			counter++;
-			
+
 			s[j + 1] = s[j];
 
-			j = j - 1;
+			j--;
 		}
-	
+
 		s[j + 1] = last;
 	}
 
@@ -40,17 +43,19 @@ int Sort::Insertion(std::string s[], int n)
 	return counter;
 }
 
-int Sort::Selection(std::string s[], int n )
+int Sort::Selection(std::string s[], int n)
 {
 	int low;
 	int counter = 0;
 	for (int i = 0; i < n; i++)
 	{
 		low = i;
-		for (int j = i + 1; j < n; j++) {
+		for (int j = i + 1; j < n; j++)
+		{
 			counter++;
-		
-			if (s[j] < s[low]) {
+
+			if (s[j] < s[low])
+			{
 				low = j;
 			}
 		}
@@ -66,57 +71,60 @@ int Sort::subMSort(std::string s[], int l, int m, int r, int cnt)
 	int len1 = m - l + 1;
 	int len2 = r - m;
 
+	std::string *left = new std::string[len1];
+	std::string *right = new std::string[len2];
 
-	std::string* left = new std::string[len1];
-	std::string* right = new std::string[len2];
-
-	for (int i = 0; i < len1; i++) {
+	for (int i = 0; i < len1; i++)
+	{
 		left[i] = s[l + i];
 	}
-	for (int j = 0; j < len2; j++) {
+	for (int j = 0; j < len2; j++)
+	{
 		right[j] = s[m + 1 + j];
 	}
 	int i = 0, j = 0;
 	int k = l;
-	while (i < len1 && j < len2) {
-	
+	while (i < len1 && j < len2)
+	{
+
 		counter++;
 		s[k++] = (left[i] < right[j]) ? left[i++] : right[j++];
 	}
-	while (i < len1) {
+	while (i < len1)
+	{
 		s[k++] = left[i++];
-	
 	}
-	while (j < len2) {
+	while (j < len2)
+	{
 		s[k++] = right[j++];
-	
 	}
 	return counter;
 }
 
-void Sort::mergeSort(std::string s[], int lInd, int rInd ,int &cnt)
+void Sort::mergeSort(std::string s[], int lInd, int rInd, int &cnt)
 {
-	
+
 	int m = 0;
-	
-	if (lInd < rInd) {
-		m =  lInd +(rInd - lInd) / 2;
+
+	if (lInd < rInd)
+	{
+		m = lInd + (rInd - lInd) / 2;
 		mergeSort(s, lInd, m, cnt);
 		mergeSort(s, (m + 1), rInd, cnt);
-		
+
 		cnt += Sort::subMSort(s, lInd, m, rInd, cnt);
-	
-	}else {
+	}
+	else
+	{
 		//int n = sizeof(*s) / sizeof(s[0]);
 		//printList(s, n);
-		
 	}
-
 }
 
 void Sort::quickSort(std::string s[], int p, int r, int &cnt)
 {
-	if (p < r) {
+	if (p < r)
+	{
 		int q = Sort::Partition(s, p, r, cnt);
 		Sort::quickSort(s, p, (q - 1), cnt);
 		Sort::quickSort(s, (q + 1), r, cnt);
@@ -127,8 +135,10 @@ int Sort::Partition(std::string s[], int p, int r, int &cnt)
 {
 	std::string x = s[r];
 	int i = p - 1;
-	for (int j = p; j < (r - 1); j++) {
-		if (s[j] <= x) {
+	for (int j = p; j < (r - 1); j++)
+	{
+		if (s[j] <= x)
+		{
 			i++;
 			cnt++;
 			Sort::swap(s[i], s[j]);
