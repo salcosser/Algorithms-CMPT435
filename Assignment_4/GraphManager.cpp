abcd167bde24035ca5ee981542f61fae24a60be7
Samuel Alcosser <samalcosser@gmail.com>
2020-11-13 18:37:05 -0500

    Setup and printing done (YAY:) )

--- Assignment_4/GraphManager.cpp
+++ Assignment_4/GraphManager.cpp
@@ -27,18 +27,12 @@ void GraphManager::Overlord(std::string fileName)
 
 				if (started)
 				{
-					std::cout << "got to the printing"<< std::endl;
+					std::cout << "got to the printing" << std::endl;
 					printAJList();
 					printMatrix();
 					linkedVect.clear();
 				}
 				started = true;
-				// do one of these for each line, and process thusly
-				// begin the next graph
-
-				// if it sees theres nothing in the vector, just continue
-				//print stuff out, and display stuff, run the searches
-				//clear vector list and print new list
 			}
 			else if (ln.find("add vertex") != std::string::npos)
 			{
@@ -46,11 +40,10 @@ void GraphManager::Overlord(std::string fileName)
 				Vertex *vertex = new Vertex();
 				vertex->id = id;
 				linkedVect.push_back(vertex);
-				//std::cout << "vertex made" << std::endl;
 			}
 			else if (ln.find("add edge") != std::string::npos)
 			{
-				
+
 				int hyph = ln.find("-");
 
 				int num1 = stoi(ln.substr((ln.find("edge") + 5), 2));
@@ -60,25 +53,16 @@ void GraphManager::Overlord(std::string fileName)
 				{
 					if (linkedVect[i]->id == num1)
 					{
-						for(int j = 0;linkedVect.size();j++){
-							if(linkedVect[j]->id == num2){
-								std::cout << "I:" << i << " J:" << j << std::endl;
-								std::cout << "element I.id:"<<linkedVect[i]->id<<" element J.id:"<<linkedVect[j]->id<<std::endl;
-								std::cout << "len"<< linkedVect.size() << std::endl;
-								
+						for (int j = 0; j < linkedVect.size(); j++)
+						{
+							if (linkedVect[j]->id == num2)
+							{
 								linkedVect[i]->neighbors.push_back(linkedVect[j]);
 								linkedVect[j]->neighbors.push_back(linkedVect[i]);
-
-
-
-
-								
-								
 							}
 						}
 					}
-				}		
-
+				}
 			}
 		}
 	}
@@ -99,36 +83,55 @@ void GraphManager::printAJList()
 		std::cout << std::endl;
 	}
 }
-void GraphManager::printMatrix(){
+void GraphManager::printMatrix()
+{
 	int size = linkedVect.size();
-	std::cout<< "about to do matrix stuff"<< std::endl;
+	std::cout << "about to do matrix stuff" << std::endl;
 	std::vector<std::vector<char>> matrix(size, std::vector<char>(size, '.'));
 	std::cout << "--Printing Matrix--" << std::endl;
-	 
+
 	//populating
-	for(int i = 0; i< linkedVect.size(); i++){
-		for(int j = 0;j<linkedVect[i]->neighbors.size(); j++ ){
-			
+	for (int i = 0; i < linkedVect.size(); i++)
+	{
+		for (int j = 0; j < linkedVect[i]->neighbors.size(); j++)
+		{
+
 			matrix[i][linkedVect[i]->neighbors[j]->id] = '1';
 		}
 	}
 	//printing
-	for(int i = 0; i< linkedVect.size(); i++){
-		std::cout << '*';
-		//table headings
-		for(int n = 0; n< linkedVect.size(); i++){
-			std::cout << linkedVect[n]->id;
+	std::cout << "    ";
+	//table headings
+	for (int n = 0; n < linkedVect.size(); n++)
+	{
+		if (linkedVect[n]->id < 10)
+		{
+			std::cout << linkedVect[n]->id  << "  ";
 		}
-		for(int n = 0; n< linkedVect.size(); i++){
-			std::cout << " - ";
+		else
+		{
+			std::cout << linkedVect[n]->id  << " ";
 		}
-		std::cout << std::endl;
+
+	}
+	std::cout << std::endl;
+
+	for (int i = 0; i < linkedVect.size(); i++)
+	{
+		if (linkedVect[i]->id < 10)
+		{
+			std::cout << " " << linkedVect[i]->id << "| ";
+		}
+		else
+		{
+			std::cout << linkedVect[i]->id << "| ";
+		}
 		//printing row by row
-		std::cout << linkedVect[i]->id << "|";
-		for(int h = 0;h<linkedVect[i]->neighbors.size(); h++ ){
-			std::cout << linkedVect[i]->neighbors[h]->id;
+
+		for (int h = 0; h < matrix[i].size(); h++)
+		{
+			std::cout << matrix[i][h] << "  ";
 		}
 		std::cout << std::endl;
 	}
-
 }
