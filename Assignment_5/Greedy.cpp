469a0771a374c73546e1656fc2bc26ed7eac457d
Samuel Alcosser <samalcosser@gmail.com>
2020-12-10 16:25:09 -0500

    Oops!

--- Assignment_5/Greedy.cpp
+++ Assignment_5/Greedy.cpp
@@ -36,7 +36,7 @@ void Greedy::setupSpices(std::string fileName)
             }
             else if (ln.find("spice name") != string::npos)
             {
-                vector<string> parts;               //keeps the data points from the line
+                vector<string> parts; //keeps the data points from the line
 
                 for (int i = 0; i < ln.size(); i++)
                 {
@@ -47,22 +47,20 @@ void Greedy::setupSpices(std::string fileName)
                             if (ln[j] == '=')
                             {
 
-                                string sub = ln.substr(j + 2, (i - j) - 2);     // found the bounds of a value, so push it on
+                                string sub = ln.substr(j + 2, (i - j) - 2); // found the bounds of a value, so push it on
 
                                 parts.push_back(sub);
-                              
+
                                 break;
                             }
                         }
                     }
                 }
 
-                Spice *nSpice = new Spice();            //make and push the spice
+                Spice *nSpice = new Spice(); //make and push the spice
                 nSpice->color = parts[0];
                 nSpice->setPrice(stod(parts[1]), stod(parts[2]));
                 spices.push_back(nSpice);
-
-               
             }
         }
     }
@@ -80,38 +78,37 @@ void Greedy::greedyAlgo(int capacity)
     };
 
     int cTotal = 0;
-    vector<Portion *> ks;       //stores the portions
-    double earnings = 0;        //stores the earnings
-    while (cTotal != capacity)
+    vector<Portion *> ks; //stores the portions
+    double earnings = 0;  //stores the earnings
+
+    // this process assumes that the items have been sorted before
+    for (int i = spices.size() - 1; i > 0; i--)
     {
-        // this process assumes that the items have been sorted before
-       for (int i = spices.size() - 1; i > 0; i--)
+
+        if ((cTotal + spices[i]->qty) <= capacity) //if the whole lot of the spice can fit, add it all to the knapsack
         {
-            if ((cTotal + spices[i]->qty) <= capacity)      //if the whole lot of the spice can fit, add it all to the knapsack
-            {
 
-                Portion *p = new Portion();
-                p->qty = spices[i]->qty;
-                p->spice = spices[i];
-                cTotal += spices[i]->qty;
-                ks.push_back(p);
-                earnings += p->spice->tPrice;
-            }
-            else if (((capacity - cTotal) > 0) && ((cTotal + spices[i]->qty) > capacity))       //if only some can fit in the knapsack, add however much will fit
-            {
+            Portion *p = new Portion();
+            p->qty = spices[i]->qty;
+            p->spice = spices[i];
+            cTotal += spices[i]->qty;
+            ks.push_back(p);
+            earnings += p->spice->tPrice;
+        }
+        else if (((capacity - cTotal) > 0) && ((cTotal + spices[i]->qty) > capacity)) //if only some can fit in the knapsack, add however much will fit
+        {
 
-                Portion *part = new Portion();
-                part->qty = capacity - cTotal;
-                part->spice = spices[i];
-                cTotal += part->qty;
-                ks.push_back(part);
-                earnings += ((part->spice->uPrice) * (part->qty));
-                break;
-            }
+            Portion *part = new Portion();
+            part->qty = capacity - cTotal;
+            part->spice = spices[i];
+            cTotal += part->qty;
+            ks.push_back(part);
+            earnings += ((part->spice->uPrice) * (part->qty));
+            break;
         }
     }
 
-    cout << "Knapsack of capacity " << capacity << " is worth " << earnings << " quatloos and" << endl;             //print out the earnings followed by a gramatically correct statement about the contents of the knapsack
+    cout << "Knapsack of capacity " << capacity << " is worth " << earnings << " quatloos and" << endl; //print out the earnings followed by a gramatically correct statement about the contents of the knapsack
     cout << "contains ";
     for (int n = 0; n < ks.size(); n++)
     {
